926f821b08633808d4bc9e648203fab4ac0a3bac
kxu07 <117796569+kxu07@users.noreply.github.com>
2026-05-19 15:28:19 -0500

    introduction draft

--- introduction.py
+++ introduction.py
@@ -7,5 +7,13 @@ st.subheader(
     "The problem and our solution"
 )
 
+st.subheader("The problem")
+st.write("In most public pool halls/pool tables, the pool cues are not constantly being maintained, so the cue tip begins to wear down and lose its dome shape as seen in Figure 1, to a flatter tip as shown in Figure 2. Most pool chalks are made with a divot in the middle that fits a dome shaped cue as shown in Figure 3. This geometric mismatch prevents flatter cue tips from being able to pick up chalk.")
+st.write("It is important to chalk your cue tip between shots so that players can utilize the friction between the cue and ball to generate spin, execute different shots, etc. More advanced pool players may avoid this issue by replacing their cue tips or carrying their own chalk, but amateurs may not know how to do so or even that they are supposed to do so. ")
+st.write("We wanted to create an intuitive and effective product that lets players chalk their cue evenly and easily, regardless of skill level.")
+st.image("dome_cuetip_intro.png", "Figure 1: Proper (dome-shaped) pool cue tip")
+st.image("flat_cuetip_intro.png", "Figure 2: Worn down (flat top) cue tip")
+st.image("chalk_intro.png", "Figure 3: Pool chalk (note dome cue tip shaped divot)")
+
 
 col1, col2 = st.columns(2)
