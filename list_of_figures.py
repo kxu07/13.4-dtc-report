1422ad088832550207d17f408a472ebfd46dcb55
kxu07 <117796569+kxu07@users.noreply.github.com>
2026-06-09 17:05:00 -0500

    fix

--- list_of_figures.py
+++ list_of_figures.py
@@ -10,12 +10,13 @@ tables = pd.DataFrame(
         "Location": ["Appendix A: Project Definition", "Appendix D: Design Review Summary", "Appendix E: Instructions for Construction"]
     }
 )
+st.table(tables)
 
 st.subheader("List of Figures")
 figures = pd.DataFrame(
     {
         "Figure": ["Figure 1: The Chalk Pop", "Figure 2: Proper (dome-shaped) pool cue tip", "Figure 3: Worn down(flat top) cue tip", "Figure 4: Pool chalk", "Figure 5: Geometric mismatch between chalk and cue tip", "Figure 6: Uneven pool chalk application w/ bald spot", "Figure 7: The Chalk Pop", "Figure 8: The Chalk Pop components", "Figure 9: Semi-spherical chalk", "Figure 10: The shroud", "Figure 11: Handle contoured to hand shape", "Figure 12: Keychain pool chalk holder", "Figure 13: Poolgods tip tool square pocket chalker", "Figure 14: Cuetex chalker and cue tip pick", "Figure 15: Universal round chalk", "Figure 16: The Chalk Pop construction", "Figure 17: Shaping chalk with bench sander", "Figure 18: The chalk sanded halfway", "Figure 19: The final round chalk", "Figure 20: The chalk in the shroud", "Figure 21: The completed Chalk Pop", "Figure 22: The recommended Chalk Pop grip"],
-        "Location": ["Executive Summary", "Introduction", "Introduction", "Introduction", "Introduction", "Introduction", "Introduction", "Design Concept and Rationale", "Design Concept and Rationale", "Design Concept and Rationale", "Design Concept and Rationale", "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix F: Instructions for Use"]
+        "Location": ["Executive Summary", "Introduction", "Introduction", "Introduction", "Introduction", "Introduction", "Introduction", "Design Concept and Rationale", "Design Concept and Rationale", "Design Concept and Rationale", "Design Concept and Rationale", "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary",  "Appendix C: Market Research Summary", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix E: Instructions for Construction", "Appendix F: Instructions for Use"]
     }
 )
 
